952fa3db9b80f7e7162df8cbdaa3e282105443e4
I.N <48551497+leztien@users.noreply.github.com>
2020-08-16 15:52:51 +0200

    Update recursive_dfs.py

--- recursive_dfs.py
+++ recursive_dfs.py
@@ -2,53 +2,62 @@
 recursive depth-first-search
 """
 
-g = \
-[(0,1,1,1,0,0,0,0,0,0),   #0
- (1,0,0,0,0,0,0,0,0,0),   #1
- (1,0,0,0,1,0,0,0,0,0),   #2
- (1,0,0,0,0,1,0,0,0,0),   #3
- (0,0,1,0,0,0,0,0,0,0),   #4
- (0,0,0,1,0,0,1,1,0,0),   #5
- (0,0,0,0,0,1,0,1,0,0),   #6
- (0,0,0,0,0,1,1,0,1,1),   #7
- (0,0,0,0,0,0,0,1,0,0),   #8
- (0,0,0,0,0,0,0,1,0,0)]   #9
-
-#display the graph
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 
-am = np.array(g)
-G = nx.from_numpy_matrix(am)
+
+#make adjacency matrix
+mx = \
+[(0,1,1,1,0,0,0,0,0,0),   #0
+ (0,0,0,0,0,0,0,0,0,0),   #1
+ (0,0,0,0,1,0,0,0,0,0),   #2
+ (0,0,0,0,0,1,0,0,0,0),   #3
+ (0,0,0,0,0,0,0,0,0,0),   #4
+ (0,0,0,0,0,0,1,1,0,0),   #5
+ (0,0,0,0,0,0,0,0,0,0),   #6
+ (0,0,0,0,0,0,0,0,1,1),   #7
+ (0,0,0,0,0,0,0,0,0,0),   #8
+ (0,0,0,0,0,0,0,0,0,0)]   #9
+# 0 1 2 3 4 5 6 7 8 9
+
+mx = np.array(mx, dtype=np.uint8)
+mx = mx | mx.T
+
+
+#vizualize the graph
+G = nx.from_numpy_matrix(mx)
 nx.draw(G, with_labels=True)
 
-#
-n = len(g)
+
+#dfs procedure
+n = len(mx)  # number fo nodes
 visited = [False,]*n
-log = []
-stack = []
+stack = []  #stack
+log = []    #optinal log list
 
-
-def dfs(ix):
-    global g, visited, log
+def dfs(start, destination=None):
+    global mx, visited, log
     
-    #log.append(ix)
+    ix = start
     
     #base case
     if visited[ix] == True:
-        #log.append(ix)
         return  # backtrack
    
     #recursive case
     visited[ix] = True #mark as visited
-    #log.append(ix)
+    log.append(ix)
+    if destination and destination == ix:
+        print("found the searched node:", ix)
     
-    neigbors = (ix for ix,v in enumerate(g[ix]) if v==1)
+    neigbors = (ix for ix,v in enumerate(mx[ix]) if v==1)
     for node in neigbors:
-        dfs(node)
+        dfs(node, destination=destination)
     
-    
+
+#test  
 start = 0
-dfs(start)
-print(log)
+destination = 9
+dfs(start, destination=9)
+print(stack, log)
